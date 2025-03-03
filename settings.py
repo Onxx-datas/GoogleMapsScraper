067d258fee45f84e1f236d758d8bd8f2d6cbfa65
Onxx-datas <abdulazizkomiljonov4621@gmail.com>
2025-03-03 13:42:24 +0500

    settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+API_KEY = ""  # API key from Places API (Google Cloud Console), (❗❗ PLACE YOURS ❗❗)
+LOCATIONS = [""]  # Longitude and Latitude from place that you want to scrape (❗❗ PLACE YOURS ❗❗)
+RADIUS = 12000  # Radius from given Longitude and Latitude (You can adjust it)  (Bigger radius = more results collected)
+KEYWORD = "Hospital"  # Searches given keyword inside the specified radius
+MAX_RESULTS = 18000  # Maximum results to collect
