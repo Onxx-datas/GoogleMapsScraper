7a30e46c1daf1ca98eddf2a8e9bc6bef4e66cf55
Onxx-datas <abdulazizkomiljonov4621@gmail.com>
2025-04-16 15:21:32 +0500

    Fixed some code blocks

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-API_KEY = ""  # API key from Places API (Google Cloud Console), (❗❗ PLACE YOURS ❗❗)
-LOCATIONS = [""]  # Longitude and Latitude from place that you want to scrape (❗❗ PLACE YOURS ❗❗)
-RADIUS = 12000  # Radius from given Longitude and Latitude (You can adjust it)  (Bigger radius = more results collected)
-KEYWORD = "Hospital"  # Searches given keyword inside the specified radius
-MAX_RESULTS = 18000  # Maximum results to collect
+API_KEY = "AIzaSyBcjel-BwPSUrZ4Bw_-Exu1hhIyOneSDuE"  # API key from Places API (Google Cloud Console), (❗❗ PLACE YOURS ❗❗)
+LOCATIONS = ["51.507351, -0.127758"]  # Longitude and Latitude from place that you want to scrape (❗❗ PLACE YOURS ❗❗)
+RADIUS = 3000  # Radius from given Longitude and Latitude (You can adjust it)  (Bigger radius = more results collected)
+KEYWORD = "restaurant"  # Searches given keyword inside the specified radius
+MAX_RESULTS = 50  # Maximum results to collect
