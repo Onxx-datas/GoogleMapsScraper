bdb6be244e73ee7b73887aaa5863a5544632c54f
Onxx-datas <abdulazizkomiljonov4621@gmail.com>
2025-04-16 15:23:35 +0500

    API removed, so you can put your own Google Places API

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-API_KEY = "AIzaSyBcjel-BwPSUrZ4Bw_-Exu1hhIyOneSDuE"  # API key from Places API (Google Cloud Console), (❗❗ PLACE YOURS ❗❗)
-LOCATIONS = ["51.507351, -0.127758"]  # Longitude and Latitude from place that you want to scrape (❗❗ PLACE YOURS ❗❗)
+API_KEY = ""  # API key from Places API (Google Cloud Console), (❗❗ PLACE YOURS ❗❗)
+LOCATIONS = [""]  # Longitude and Latitude from place that you want to scrape (❗❗ PLACE YOURS ❗❗)
 RADIUS = 3000  # Radius from given Longitude and Latitude (You can adjust it)  (Bigger radius = more results collected)
-KEYWORD = "restaurant"  # Searches given keyword inside the specified radius
+KEYWORD = ""  # Searches given keyword inside the specified radius
 MAX_RESULTS = 50  # Maximum results to collect
